44303fb8ca4acb6c0e956abc855c0aa5a50d70bb
Jia Jia <mathjiajia@outlook.com>
2025-03-07 22:28:49 +0800

    move

--- plugin/latex-conceal.lua
+++ plugin/latex-conceal.lua
@@ -1,1 +1,25 @@
-require("latex-conceal").setup()
+local defaults = {
+	enabled = {
+		"amssymb",
+		"core",
+		"delim",
+		"font",
+		"greek",
+		"math",
+		-- "mleftright",
+		"script",
+	},
+	add = {},
+}
+
+print("here")
+
+local conf = vim.g.latex_conf or {}
+conf = vim.tbl_deep_extend("force", defaults, conf)
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.md", "*.markdown", "*.tex" },
+	group = vim.api.nvim_create_augroup("latex-conceal", {}),
+	callback = function()
+		require("latex-conceal.conceals").init(conf)
+	end,
+})
